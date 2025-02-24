4b8f8f0c9c6c0972cae8c09bd5fed813146113e6
Ethan McLaughlin <McLaughlin-E66@ulster.ac.uk>
2025-02-24 11:51:07 +0000

    Player movement

---
+++ Assets/Scripts/GridMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Transform movePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position 
+
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+        {
+            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        }
+
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+        {
+            movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+        }
+    }
+}
