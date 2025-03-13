0c6096ea13a60284ab2d6e9fb0f1f80649221bd3
Kathryn Douglas <Douglas-K6@ulster.ac.uk>
2025-03-13 16:22:20 +0000

    Update Animation Code

--- Assets/Scripts/AnimationsCode.cs
+++ Assets/Scripts/AnimationsCode.cs
@@ -7,7 +7,12 @@ public class SpriteMover : MonoBehaviour
     public float movementSpeed = 5f;
     public Transform movePoint;
     public LayerMask whatStopsMovement;
-
+    public bool isNearCrate;
+    public bool isPushingUp;
+    public bool isPushingLeft;
+    public bool isPushingDown;
+    public bool isPushingRight;
+    
     public Animator animator;
     void Start()
     {
@@ -20,101 +25,109 @@ public class SpriteMover : MonoBehaviour
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
+        if (isNearCrate && Input.GetKeyDown(KeyCode.W))
+        {
 
-        if (Input.GetKeyDown(KeyCode.W))
+            animator.SetBool("IsPushingUp", true);
+            isPushingUp = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+
+            animator.SetBool("IsPushingUp", false);
+            isPushingUp = false;
+        }
+
+        if (!isPushingUp && Input.GetKey(KeyCode.W))
         {
             animator.SetBool("IsWalkingUp", true);
         }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            animator.SetBool("IsWalkingLeft", true);
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            animator.SetBool("IsWalkingDown", true);
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            animator.SetBool("IsWalkingRight", true);
-        }
-
-        if (Input.GetKeyUp(KeyCode.W))
+        else if (Input.GetKeyUp(KeyCode.W))
         {
             animator.SetBool("IsWalkingUp", false);
         }
 
-        if (Input.GetKeyUp(KeyCode.A))
+        if (isNearCrate && Input.GetKeyDown(KeyCode.A))
+        {
+
+            animator.SetBool("IsPushingLeft", true);
+            isPushingLeft = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+
+            animator.SetBool("IsPushingLeft", false);
+            isPushingLeft = false;
+        }
+
+        if (!isPushingLeft && Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("IsWalkingLeft", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
         {
             animator.SetBool("IsWalkingLeft", false);
         }
 
+        if (isNearCrate && Input.GetKeyDown(KeyCode.S))
+        {
 
-        if (Input.GetKeyUp(KeyCode.S))
+            animator.SetBool("IsPushingDown", true);
+            isPushingDown = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+
+            animator.SetBool("IsPushingDown", false);
+            isPushingDown = false;
+        }
+
+        if (!isPushingDown && Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("IsWalkingDown", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
         {
             animator.SetBool("IsWalkingDown", false);
         }
 
-        if (Input.GetKeyUp(KeyCode.D))
+        if (isNearCrate && Input.GetKeyDown(KeyCode.D))
+        {
+
+            animator.SetBool("IsPushingRight", true);
+            isPushingRight = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+
+            animator.SetBool("IsPushingRight", false);
+            isPushingRight = false;
+        }
+
+        if (!isPushingRight && Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("IsWalkingRight", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
         {
             animator.SetBool("IsWalkingRight", false);
         }
-
     }
 
-   
-   /* {
-        public Animator animator;
-        public bool isNearCrate;  // A flag to check if the player is near a crate
-        private bool isPushing;   // A flag to check if the player is pushing the crate
-
-        void Update()
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Crate"))
         {
-            // Check if the player is near a crate (for this example, assuming isNearCrate is set elsewhere)
-            if (isNearCrate && Input.GetKeyDown(KeyCode.W))
-            {
-                // Trigger pushing animation
-                animator.SetBool("IsPushing", true);  // Assuming the animator has a boolean parameter "IsPushing"
-                isPushing = true;
-            }
-            else if (Input.GetKeyUp(KeyCode.W))
-            {
-                // Stop pushing animation
-                animator.SetBool("IsPushing", false);
-                isPushing = false;
-            }
-
-            // Check for walking animation if not pushing
-            if (!isPushing && Input.GetKey(KeyCode.W))
-            {
-                animator.SetBool("IsWalkingUp", true);
-            }
-            else if (Input.GetKeyUp(KeyCode.W))
-            {
-                animator.SetBool("IsWalkingUp", false);
-            }
+            isNearCrate = false;
         }
+    }
 
-        // This function could be triggered when the player is near a crate
-        // For example, using a trigger zone, raycast, or collision detection
-        private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Crate"))
         {
-            if (other.CompareTag("Crate"))
-            {
-                isNearCrate = true;
-            }
+            isNearCrate = true;
         }
+    }
 
-        private void OnTriggerExit(Collider other)
-        {
-            if (other.CompareTag("Crate"))
-            {
-                isNearCrate = false;
-            }
-        }*/
-    } 
-   
 }
