fa3befc29a6e86c0d8c07d45eb4f27f8a0b477b9
Caleb Allen <Allen-C49@ulster.ac.uk>
2025-04-01 15:36:42 +0100

    InventoryAddedtoWarehouse1

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,20 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class InventoryManager : MonoBehaviour
 {
-    public static InventoryManager Instance;
+    public static InventoryManager Instance; // Singleton reference
     public event Action OnInventoryUpdated;
-
     private List<Sprite> collectedItems = new List<Sprite>();
 
     private void Awake()
     {
         if (Instance == null)
+        {
             Instance = this;
+            Debug.Log("InventoryManager Instance is set.");
+        }
         else
+        {
+            Debug.LogWarning("Duplicate InventoryManager found! Destroying this one.");
             Destroy(gameObject);
+        }
     }
 
     public void AddItem(Sprite itemSprite)
@@ -23,20 +28,12 @@ public class InventoryManager : MonoBehaviour
         {
             collectedItems.Add(itemSprite);
             Debug.Log("Item added to inventory: " + itemSprite.name);
-
-            OnInventoryUpdated?.Invoke(); // Trigger UI Update
+            OnInventoryUpdated?.Invoke();
         }
     }
 
     public List<Sprite> GetCollectedItems()
     {
-        Debug.Log("Inventory contains " + collectedItems.Count + " items.");
-        foreach (Sprite sprite in collectedItems)
-        {
-            Debug.Log("- " + sprite.name);
-        }
         return collectedItems;
     }
-
-    
 }
