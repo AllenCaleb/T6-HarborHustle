92a728b95eef5b834b174d54144616ee1682a8fa
Caleb Allen <Allen-C49@ulster.ac.uk>
2025-04-01 12:58:12 +0100

    implementing new inventory system

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,23 +1,33 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class InventoryManager : MonoBehaviour
 {
-    public static InventoryManager instance;
-    public List<Sprite> inventorySprites = new List<Sprite>(); // Stores item images
+    public static InventoryManager Instance;
+    public event Action OnInventoryUpdated;
+
+    private List<Sprite> collectedItems = new List<Sprite>();
 
     private void Awake()
     {
-        if (instance == null)
-            instance = this;
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
     }
 
     public void AddItem(Sprite itemSprite)
     {
-        if (!inventorySprites.Contains(itemSprite))
+        if (!collectedItems.Contains(itemSprite))
         {
-            inventorySprites.Add(itemSprite);
-            UIManager.instance.UpdateUI(itemSprite); // Update UI with image
+            collectedItems.Add(itemSprite);
+            OnInventoryUpdated?.Invoke();  // Notify that the inventory has been updated
         }
+    }
+
+    public List<Sprite> GetCollectedItems()
+    {
+        return collectedItems;
     }
 }
