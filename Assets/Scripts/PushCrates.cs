d2b08b31898a2796e852fb0eab1b4b422cb4548c
Kathryn Douglas <Douglas-K6@ulster.ac.uk>
2025-04-14 16:19:01 +0100

    tryna make boxes move

--- Assets/Scripts/PushCrates.cs
+++ Assets/Scripts/PushCrates.cs
@@ -17,7 +17,7 @@ public class PushCrates : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+  /*  void Update()
     {
         Physics2D.queriesStartInColliders = false;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
@@ -42,5 +42,5 @@ public class PushCrates : MonoBehaviour
         Gizmos.color = Color.yellow;
          
         Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * distance);
-    }
+    }*/
 }
